5a0ba2af6e779c1eaf0a41602ed7437d0b1b5a74
Your Name <you@example.com>
2019-02-11 17:33:33 -0500

    1

---
+++ src/test/java/org/junit/jupiter/api/Patterntest.java
@@ -0,0 +1,14 @@
+package org.junit.jupiter.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Patterntest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
